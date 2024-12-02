4b5c352630510cb901302fd1186f69a5f6bb9af8
Xenofon Foukas <xefouk@microsoft.com>
2024-12-02 11:09:01 +0000

    Fixes when unloading program

--- das/unload_prog.sh
+++ das/unload_prog.sh
@@ -2,7 +2,14 @@
 
 ip link set dev enp63s0v0 xdp off
 ip link set dev enp63s0v1 xdp off
-rm /sys/fs/bpf/das
-rm /sys/fs/bpf/das2
-rm /sys/fs/bpf/das_redirect_map
-rm /sys/fs/bpf/das2_redirect_map
+ip link set dev enp63s0v2 xdp off
+ip link set dev enp63s0v3 xdp off
+rm /sys/fs/bpf/das_egress
+rm /sys/fs/bpf/das_egress2
+rm /sys/fs/bpf/das_ingress
+rm /sys/fs/bpf/das_ingress2
+
+rm /sys/fs/bpf/das_redirect_map_egress
+rm /sys/fs/bpf/das_redirect_map_egress2
+rm /sys/fs/bpf/das_redirect_map_ingress
+rm /sys/fs/bpf/das_redirect_map_ingress2
