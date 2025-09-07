1ed3738efd3beaec169af4d09395848c62092b57
Xenofon Foukas <xefouk@microsoft.com>
2025-09-07 13:48:30 +0000

    Added real-time prb monitoring to cmake

--- src/xdp/prb_mon/prb_mon_egress.c
+++ src/xdp/prb_mon/prb_mon_egress.c
@@ -7,6 +7,10 @@
 
 #include "ranbooster_common.h"
 
+/*
+ This module handles the egress traffic, i.e., traffic going from the DU to the RUs.
+*/
+
 #define VLAN_VID_MASK    0x0FFF
 
 __u8 booster_mac_addr[] = {0x00, 0x00, 0x11, 0x11, 0x22, 0x22};
@@ -27,14 +31,14 @@ struct {
     __uint(max_entries, 1);          
     __type(key, __u32);              
     __type(value, __u16); 
-} du_vlan SEC(".maps");
+} ru_vlan SEC(".maps");
 
 struct {
     __uint(type, BPF_MAP_TYPE_ARRAY);
     __uint(max_entries, 1);          
     __type(key, __u32);              
     __type(value, __u16); 
-} ru_vlan SEC(".maps");
+} du_vlan SEC(".maps");
 
 struct {
     __uint(type, BPF_MAP_TYPE_ARRAY);
@@ -82,7 +86,7 @@ int xdp_prb_mon(struct xdp_md *ctx) {
     __u32 key = 0;
     __u16 symbol = 0;
     __u8 *iq_samples_head;
-    unsigned int num_prbs;
+    unsigned int num_prbs = 0;
     
     // Get a pointer to the packet's data and data_end
     void *data = (void *)(long)ctx->data;
@@ -134,12 +138,13 @@ int xdp_prb_mon(struct xdp_md *ctx) {
 
         vlan_id = vlan_tci & VLAN_VID_MASK;
 
-        if (vlan_id != *ru_vlan_num) {
+        if (vlan_id != *du_vlan_num) {
             return XDP_DROP;
         }
         
         next_hdr = (__u8 *)next_hdr + sizeof(struct vlan_hdr);
         ether_type = bpf_ntohs(vlan_h->h_vlan_encapsulated_proto);
+
     } else if (ether_type == 44798) { // Packets we get from loopback can be dropped
         return XDP_DROP;   
     }
@@ -153,104 +158,111 @@ int xdp_prb_mon(struct xdp_md *ctx) {
     __u8 ecpri_message_type = ecpri_hdr->cmnhdr.bits.ecpri_mesg_type;
     __u16 ru_port_id = bpf_ntohs(ecpri_hdr->ecpri_xtc_id) & 0x000F;
 
-    // Check if this packet is coming from the correct RU
-    if (bpf_memcmp(ru_mac, eh->h_source, ETH_ALEN) == 0) {
-    
-        __builtin_memcpy(eh->h_dest, ranbooster_du_mac, ETH_ALEN);
-        __builtin_memcpy(eh->h_source, booster_mac_addr, ETH_ALEN);
+    // Check if this packet is coming from the DU
+    if (bpf_memcmp(ranbooster_du_mac, eh->h_source, ETH_ALEN) == 0) {
 
-        if (vlan_h) {
-            if ((void *)(vlan_h + 1) > data_end) {                        
-                return XDP_DROP;
-            }
-            vlan_tci = (vlan_tci & 0xF000) | (*du_vlan_num & 0x0FFF);
-            vlan_h->h_vlan_TCI = bpf_htons(vlan_tci);
-        }
+        // Check if this is going to the middlebox
+        if (bpf_memcmp(booster_mac_addr, eh->h_dest, ETH_ALEN) == 0) {
+            // Change the MAC address to send to the correct destination
+            __builtin_memcpy(eh->h_dest, ru_mac, ETH_ALEN);
+            __builtin_memcpy(eh->h_source, du_mac, ETH_ALEN);
 
-        return XDP_TX;
-
-        if (ecpri_message_type == ECPRI_IQ_DATA) {
-
-            if (ru_port_id != SELECTED_RU_PORT_ID) {
-                return XDP_TX;
-            }
-
-            next_hdr = (__u8 *)next_hdr + sizeof(struct xran_ecpri_hdr);
-
-            struct radio_app_common_hdr *app_common_hdr = (struct radio_app_common_hdr *)next_hdr;
-
-            if ((void *)(app_common_hdr + 1) > data_end) {
-                return XDP_DROP;
-            }
-
-            struct radio_app_common_hdr radio_hdr_cpy = *app_common_hdr;
-                
-            radio_hdr_cpy.sf_slot_sym.value = bpf_ntohs(radio_hdr_cpy.sf_slot_sym.value);
-            symbol = radio_hdr_cpy.sf_slot_sym.symb_id;
-
-            if (symbol != MONITORING_SYMBOL_INGRESS) {
-                return XDP_TX;
-            }
-
-            next_hdr = (__u8 *)next_hdr + sizeof(struct radio_app_common_hdr);
-            struct data_section_hdr *data_sec_hdr = (struct data_section_hdr *)next_hdr;
-
-            if ((void *)(data_sec_hdr + 1) > data_end) {
-                return XDP_DROP;
-            }
-
-            struct data_section_hdr data_sec_hdr_cpy = *data_sec_hdr;
-            data_sec_hdr_cpy.fields.all_bits = bpf_ntohl(data_sec_hdr->fields.all_bits);
-            num_prbs = data_sec_hdr_cpy.fields.num_prbu;
-
-            if (num_prbs == 0) {
-                num_prbs = 273;
-            }
-
-            next_hdr = (__u8 *)next_hdr + sizeof(struct data_section_hdr);
-
-            struct data_section_compression_hdr *cmp_header = (struct data_section_compression_hdr *)next_hdr;
-
-            if ((void *)(cmp_header + 1) > data_end) {
-                return XDP_DROP;
-            }
-
-            iq_samples_head = next_hdr + sizeof(struct data_section_compression_hdr);
-
-            struct prb_stats_event *e = bpf_ringbuf_reserve(&comp_load, sizeof(struct prb_stats_event), 0);
-            
-            if (!e)
-                return XDP_TX;
-
-            for (int rb_id = 0; rb_id < MAX_NUM_RBS; rb_id++) {
-                    
-                if (rb_id >= num_prbs) {
-                    break;
+            if (vlan_h) {
+                if ((void *)(vlan_h + 1) > data_end) {                        
+                    return XDP_DROP;
                 }
-
-                if ((void *)(iq_samples_head + COMPRESSED_RB_SIZE_BYTES) > data_end) {
-                    break;
-                }
-
-                struct compression_params *comp_params = (struct compression_params *)iq_samples_head;
-
-                e->total_num_prbs++;
-
-                if (comp_params->exponent > 2) {
-                    e->num_prbs_used += 1;
-                }
-
-                iq_samples_head += COMPRESSED_RB_SIZE_BYTES; // 28B for each RB
+                vlan_tci = (vlan_tci & 0xF000) | (*ru_vlan_num & 0x0FFF);
+                vlan_h->h_vlan_TCI = bpf_htons(vlan_tci);
             }
-
-            bpf_ringbuf_submit(e, 0);
 
             return XDP_TX;
-        } else {
-            return XDP_DROP;
+
+            if (ecpri_message_type == ECPRI_RT_CONTROL_DATA) { // If this is a C-plane message
+                return XDP_TX;
+            } else if (ecpri_message_type == ECPRI_IQ_DATA) {
+
+                if (ru_port_id != SELECTED_RU_PORT_ID) {
+                    return XDP_TX;
+                }
+
+                next_hdr = (__u8 *)next_hdr + sizeof(struct xran_ecpri_hdr);
+
+                struct radio_app_common_hdr *app_common_hdr = (struct radio_app_common_hdr *)next_hdr;
+
+                if ((void *)(app_common_hdr + 1) > data_end) {
+                    return XDP_DROP;
+                }
+
+                struct radio_app_common_hdr radio_hdr_cpy = *app_common_hdr;
+                
+                radio_hdr_cpy.sf_slot_sym.value = bpf_ntohs(radio_hdr_cpy.sf_slot_sym.value);
+                symbol = radio_hdr_cpy.sf_slot_sym.symb_id;
+
+                if (symbol != MONITORING_SYMBOL_EGRESS) {
+                    return XDP_TX;
+                }
+
+                next_hdr = (__u8 *)next_hdr + sizeof(struct radio_app_common_hdr);
+                struct data_section_hdr *data_sec_hdr = (struct data_section_hdr *)next_hdr;
+
+                if ((void *)(data_sec_hdr + 1) > data_end) {
+                    return XDP_DROP;
+                }
+
+                struct data_section_hdr data_sec_hdr_cpy = *data_sec_hdr;
+                data_sec_hdr_cpy.fields.all_bits = bpf_ntohl(data_sec_hdr->fields.all_bits);
+                num_prbs = data_sec_hdr_cpy.fields.num_prbu;
+
+                if (num_prbs == 0) {
+                    num_prbs = 273;
+                }
+
+                next_hdr = (__u8 *)next_hdr + sizeof(struct data_section_hdr);
+
+                struct data_section_compression_hdr *cmp_header = (struct data_section_compression_hdr *)next_hdr;
+
+                if ((void *)(cmp_header + 1) > data_end) {
+                    return XDP_DROP;
+                }
+
+                iq_samples_head = next_hdr + sizeof(struct data_section_compression_hdr);
+
+                struct prb_stats_event *e = bpf_ringbuf_reserve(&comp_load, sizeof(struct prb_stats_event), 0);
+
+                if (!e) {
+                    return XDP_TX;
+                }
+                    
+                for (int rb_id = 0; rb_id < MAX_NUM_RBS; rb_id++) {
+                    
+                    if (rb_id >= num_prbs) {
+                        break;
+                    }
+
+                    if ((void *)(iq_samples_head + COMPRESSED_RB_SIZE_BYTES) > data_end) {
+                        break;
+                    }
+
+                    union compression_params *comp_params = (union compression_params *)iq_samples_head;
+
+                    e->total_num_prbs++;
+
+                    if (comp_params->blockFlPoint.exponent > 0) {
+                        e->num_prbs_used += 1;
+                    }
+
+                    iq_samples_head += COMPRESSED_RB_SIZE_BYTES; // 28B for each RB
+                }
+
+                bpf_ringbuf_submit(e, 0);
+
+                return XDP_TX;
+            } else {
+                return XDP_DROP;
+            }
         }
     }
     return XDP_DROP;
 }
 
-char _license[] SEC("license") = "MIT";
+char _license[] SEC("license") = "GPL";
