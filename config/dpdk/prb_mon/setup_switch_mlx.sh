7a00f067303c51da8b6866f628b4b764d163caed
Xenofon Foukas <xefouk@microsoft.com>
2025-09-09 22:59:03 +0000

    Added configs for DPDK prb monitor

--- config/dpdk/prb_mon/setup_switch_mlx.sh
+++ config/dpdk/prb_mon/setup_switch_mlx.sh
@@ -4,14 +4,14 @@ echo 3 > /sys/class/net/eth0/device/sriov_numvfs
 
 ip link set eth2 vf 0 mac aa:bb:cc:11:22:33 spoofchk off
 
-
 ip link set enp75s0np0 vf 0 trust on
 ip link set enp75s0np0 vf 1 trust on
 
+#DU interface
 ip link set enp75s0np0 vf 0 mac 00:00:11:11:22:22 spoofchk off
 ip link set enp75s0v0 address 00:00:11:11:22:22
 
-# RU 7
+# RU interface
 ip link set enp75s0np0 vf 1 mac 00:11:22:33:0B:6E spoofchk off
 ip link set enp75s0v1 address 00:11:22:33:0B:6E
 
